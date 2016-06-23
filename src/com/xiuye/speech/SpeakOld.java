f6362f3c87c593009739f5c2619a0808eb49067c
xiuye <xiuye_engineer@outlook.com>
2016-06-23 15:42:49 +0800

    old

--- src/com/xiuye/speech/SpeakOld.java
+++ src/com/xiuye/speech/SpeakOld.java
@@ -7,6 +7,20 @@ import java.io.InputStream;
 
 public class SpeakOld {
 
+	/**
+	 * 以下方式会产生 错误 :首次加载的时候:
+	 * C:\Users\Administrator\Desktop>java -jar speech.jar Exception in
+	 * thread "main" java.lang.UnsatisfiedLinkError: C:\Users\Administrato
+	 * r\Desktop\Speak.dll: 另一个程序正在使用此文件，进程无法访问。 at
+	 * java.lang.ClassLoader$NativeLibrary.load(Native Method) at
+	 * java.lang.ClassLoader.loadLibrary0(ClassLoader.java:1941) at
+	 * java.lang.ClassLoader.loadLibrary(ClassLoader.java:1857) at
+	 * java.lang.Runtime.loadLibrary0(Runtime.java:870) at
+	 * java.lang.System.loadLibrary(System.java:1122) at
+	 * com.xiuye.speech.Speak.<clinit>(Speak.java:30) at
+	 * com.xiuye.speech.test.SpeakTest.main(SpeakTest.java:9)
+	 * 
+	 */
 	static {
 		String OSArch = System.getProperty("sun.arch.data.model");
 		InputStream in = null;
@@ -16,17 +30,16 @@ public class SpeakOld {
 			if ("32".equals(OSArch)) {
 				File fSpeakDll = new File("Speak.dll");
 				if (!fSpeakDll.exists()) {
-					
-					
+
 					in = ClassLoader.getSystemResourceAsStream("Speak.dll");
-					
+
 					byte[] data = new byte[1024];
 					out = new FileOutputStream("Speak.dll", false);
 					while (in.read(data) != -1) {
 						out.write(data);
 						out.flush();
 					}
-					
+
 				}
 				System.loadLibrary("Speak");
 				// Runtime.getRuntime().loadLibrary("Speak");
@@ -42,7 +55,7 @@ public class SpeakOld {
 						out.write(data);
 						out.flush();
 					}
-					
+
 				}
 				System.loadLibrary("SpeakX64");
 			}
