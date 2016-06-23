1560658dd20be4f1278c45568c633b27ed2d14e6
xiuye <xiuye_engineer@outlook.com>
2016-06-23 13:18:22 +0800

    complete compile win 64 dll

--- src/com/xiuye/speech/Speak.java
+++ src/com/xiuye/speech/Speak.java
@@ -2,30 +2,46 @@ package com.xiuye.speech;
 
 public class Speak {
 
-	static{
-		System.loadLibrary("Speak");
+	static {
+		String OSArch = System.getProperty("sun.arch.data.model");
+		//jvm 32载入
+		if ("32".equals(OSArch)) {
+			System.loadLibrary("Speak");
+			//Runtime.getRuntime().loadLibrary("Speak");
+		}
+		//jvm 64载入
+		else{
+			System.loadLibrary("SpeakX64");
+		}
 	}
-	
-	
+
 	public native void coInitialize();
+
 	public native void speak(String msg);
+
 	public native void coUninitialize();
-	
-	public native void setSpeed(int speed);//rate 10 -10
+
+	public native void setSpeed(int speed);// rate 10 -10
+
 	public native int getSpeed();
+
 	public native void pause();
+
 	public native void resume();
+
 	public native int kindsOfVoice();
+
 	/**
 	 * 如果说不了中文说明只能说英语
+	 * 
 	 * @param kindOfVoice
 	 */
 	public native void setVoice(int kindOfVoice);
+
 	public native int getVoice();
+
 	public native void setVolume(int size);
+
 	public native int getVolume();
-	
-	
-	
-	
+
 }
